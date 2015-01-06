14f445f505be0314f97831fc480e353c56035551
Joshua Hancock <jhancock1229@gmail.com>
2015-01-06 12:23:10 -0700

    cut out some redundant code.

--- Exercise1/exercise_1.py
+++ Exercise1/exercise_1.py
@@ -1,15 +1,12 @@
 first_int = raw_input("Please enter the first integer: ")
 while not first_int.isdigit():
     first_int = raw_input("That is not an integer, please enter an integer: ")
-    print first_int
 second_int = raw_input("Please enter the second integer: ")
 while not second_int.isdigit() or second_int == "0":
-    if int(second_int) == 0:
+    if second_int == "0":
         second_int = (raw_input("You cannot divide by zero! Pick a different value: "))
-        print "You cannot divide by zero! Pick a different value: "
     else:
         second_int = (raw_input("That is not an integer, please enter an integer: "))
-        print second_int
 add = int(first_int) + int(second_int)
 subtract = int(first_int) - int(second_int)
 multiply = int(first_int) * int(second_int)
