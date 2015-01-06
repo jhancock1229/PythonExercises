75d13c6701949d6bc7d1cd66e186a686efa958f4
Joshua Hancock <jhancock1229@gmail.com>
2015-01-06 09:53:33 -0700

    This is exercise 2 without solving issue of entering string

--- Exercise2/exercise_2.py
+++ Exercise2/exercise_2.py
@@ -1,3 +1,14 @@
-x = int(raw_input("Please enter the number of gallons of gasoline: "))
-print "Original number of gallons is: ", x
-liters = x * 3.7854
+user_input = int(raw_input("Please enter the number of gallons of gasoline: "))
+liters = user_input * 3.7854
+gas_barrel = user_input / 42
+oil_barrel = gas_barrel / 19.5
+lbs_COO = user_input * 20
+BTU = user_input * 115000
+cost = user_input * 4
+ethanol_gallon = user_input * (115000 / 75700)
+print "Original number of gallons is: %s" % (user_input)
+print "%s gallons of gasoline requires %s liters" % (user_input, liters)
+print "%s gallons of gasoline produces %s barrels of oil" % (user_input, oil_barrel)
+print "%s gallons of gasoline produces %s pounds of CO2" % (user_input, lbs_COO)
+print "%s gallons of gasoline is energy equivalent to %s gallons of ethanol" % (user_input, ethanol_gallon)
+print "%s gallons of gasoline requires %s US dollars" % (user_input, cost)
