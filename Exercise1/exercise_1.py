f2894d666184ef6f37a7ca9b16e58a4829358ae1
Joshua Hancock <jhancock1229@gmail.com>
2015-01-05 21:09:36 -0700

    Updated function and variable names to be more specific

--- Exercise1/exercise_1.py
+++ Exercise1/exercise_1.py
@@ -1,19 +1,19 @@
-x = int(raw_input("Please enter the first integer: "))
-y = int(raw_input("Please enter the second integer: "))
-a = x + y
-b = x - y
-c = x * y
-def d(x,y):
+first_int = int(raw_input("Please enter the first integer: "))
+second_int = int(raw_input("Please enter the second integer: "))
+add = first_int + second_int
+subtract = first_int - second_int
+multiply = first_int * second_int
+def division(x, y):
     if y == 0:
         return "You cannot divide by zero!"
     else:
-        return x / y
-def e(x,y):
+        return first_int / second_int
+def remainder(x, y):
     if y == 0:
         return "You cannot divide by zero!"
     else:
-        return x % y
-print "The sum of" ,x, "and" ,y, "is: " , a
-print "The difference of" ,x, "and" ,y, "is: " , b
-print "The product of" ,x, "and" ,y, "is: " , c
-print "The quotient of" ,x, "and" ,y, "is: " , d(x,y) , "with remainder: " , e(x,y)
+        return first_int % second_int
+print "The sum of", first_int, "and", second_int, "is: ", add
+print "The difference of", first_int, "and", second_int, "is: ", subtract
+print "The product of", first_int, "and", second_int, "is: ", multiply
+print "The quotient of", first_int, "and", second_int, "is: ", division(first_int, second_int), "with remainder: ", remainder(first_int, second_int)
---
+++ Exercise2/exercise_2.py
@@ -0,0 +1,3 @@
+x = int(raw_input("Please enter the number of gallons of gasoline: "))
+print "Original number of gallons is: ", x
+liters = x * 3.7854
