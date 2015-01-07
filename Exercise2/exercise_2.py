26c829612fd407b9e0d57fe55f2fa60b9bb964ed
Joshua Hancock <jhancock1229@gmail.com>
2015-01-07 14:34:56 -0700

    Fixed issue when user inputted anything but a number

--- Exercise2/exercise_2.py
+++ Exercise2/exercise_2.py
@@ -1,19 +1,28 @@
 # Initial user input
-user_input = int(raw_input("Please enter the number of gallons of gasoline: "))
+user_input = raw_input("Please enter the number of gallons of gasoline: ")
+
+
+# Prompts user to enter numerical value if they failed to do so initially.
+while not user_input.isdigit():
+    user_input = raw_input("That wasn't a number! Please enter a number: ")
+
+
+# Converts user input from a string to an integer
+user_gallon = int(user_input)
 
 
 # Variables which convert user_input into unit conversions
-liters = user_input * 3.7854
-gas_barrel = user_input / 42
-oil_barrel = gas_barrel / 19.5
-lbs_COO = user_input * 20
-BTU = user_input * 115000
-cost = user_input * 4
-ethanol_gallon = user_input * (115000 / 75700)
+liters = user_gallon * 3.7854
+oil_barrel = user_gallon / 19.5
+lbs_COO = user_gallon * 20
+BTU = user_gallon * 115000
+cost = user_gallon * 4
+gas_to_ethanol_ratio = float(115000) / float(75700)
+ethanol_gallon = user_gallon * gas_to_ethanol_ratio
 
 
 # Execution of program
-print "Original number of gallons is: %s" % (user_input)
+print "Original number of gallons is: %s" % user_input
 print "%s gallons of gasoline requires %s liters" % (user_input, liters)
 print "%s gallons of gasoline produces %s barrels of oil" % (user_input, oil_barrel)
 print "%s gallons of gasoline produces %s pounds of CO2" % (user_input, lbs_COO)
