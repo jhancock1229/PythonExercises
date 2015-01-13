250339dde203b43f2e3f7377abb645a4c95f94cc
Joshua Hancock <jhancock1229@gmail.com>
2015-01-12 20:40:26 -0700

    Finished the exercise

--- Exercise6/exercise_6.py
+++ Exercise6/exercise_6.py
@@ -2,5 +2,17 @@ def function_one(file_name):
     with open(file_name, 'r') as f:
         return f
 
-
 def function_two(file_name):
+    try:
+        function_one(file_name)
+    except IOError:
+        print "Error: can't find file or read data."
+    else:
+        print "Written content in the file successfully!"
+
+
+print function_two('list.txt')
+
+
+
+
