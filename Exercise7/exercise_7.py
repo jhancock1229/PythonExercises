3ee948e9feb16cb3de93a78832ec8822a3f03e92
Joshua Hancock <jhancock1229@gmail.com>
2015-01-16 11:42:54 -0700

    Adding ex7

---
+++ Exercise7/exercise_7.py
@@ -0,0 +1,80 @@
+import conversions
+
+
+city_temp = {
+    "Boston": "0 C",
+    "Boise": "48 F",
+    "Phoenix": "85 F",
+    "Miami": "40 C",
+    "Riverside": "30 C",
+    "Baltimore": "32 F"
+}
+
+
+def value_list(city_temp):
+    new_list = city_temp.items()
+    return new_list
+
+
+def convert_to_integer(new_list):
+    temp_as_int = int(value_list(new_list[0]))
+    return temp_as_int
+
+
+def unit_convert(new_list, temp_as_int):
+    if new_list[-1] == "F":
+        return conversions.f_to_c(temp_as_int)
+    elif new_list[-1] == "C":
+        return conversions.c_to_f(temp_as_int)
+
+
+a = value_list(city_temp)
+print a
+
+# def assignment():
+#     for temp in city_temp.values():
+#         c = unit_convert(temp)
+#         return c
+
+
+
+
+
+# def convert_temp():
+#     temp_unit = determine_temp_unit()
+#     value_list = city_temp_value_list()
+#     if temp_unit == "F":
+#         conversions.f_to_c(value_list)
+
+# def city_temp_converted(new_list, temp_init):
+#     city_temp2 = {}
+#     new_values = determine_temp_unit(new_list, temp_int)
+#     for line in new_values:
+#     return new_values
+
+
+# def city_temp_value_list():
+#     for city in city_temp:
+#         new_list = city_temp[city].split()
+#         temp_int = int(new_list[0])
+#         determine_temp_unit(new_list, temp_int)
+
+
+
+# def assignment(city_temp):
+#     for city, temp in city_temp.items():
+#         print "In %s it is %s," % (city, temp)
+#         # print "which is equivalent to %s" % ()
+#
+# assignment(city_temp)
+
+
+
+
+
+
+
+
+
+
+
